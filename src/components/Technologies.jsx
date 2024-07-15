4643e701432ded935414cd0873113d0932f47605
zaid-sinpie <zaidmasuldar@gmail.com>
2024-07-15 14:11:30 +0530

    techblock started

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import TechBlock from './ui/TechBlock.jsx';
 
 const Technologies = () => {
   return (
     <figure className='border border-borderColor rounded-xl px-4 py-4 flex flex-wrap gap-2 items-start justify-start'>
-        
+      <TechBlock />
+      <TechBlock />
+      <TechBlock />
+      <TechBlock />
     </figure>
   )
 }
--- src/components/ui/TechBlock.jsx
+++ src/components/ui/TechBlock.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const TechBlock = () => {
   return (
-    <figure className='flex flex-col border border-borderColor rounded-xl text-textColor'>
+    <figure className='flex flex-col items-center justify-around border border-borderColor rounded-lg text-textColor w-[40%] max-sm:w-full'>
         <h1>Header</h1>
         <div className='flex gap-2 items-center justify-start'>
+          
         </div>
     </figure>
   )
